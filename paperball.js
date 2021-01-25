ddb9e8b5aa1b06282adcdfbf25c41671d671731a
Purab07 <73265862+Purab07@users.noreply.github.com>
2021-01-24 19:12:51 -0800

    Add files via upload

---
+++ paperball.js
@@ -0,0 +1,19 @@
+class Paperball{
+    constructor(x,y,width,height){
+        var options={
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+        this.body = Bodies.ellipse(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position;
+        ellipseMode(CENTER);
+        ellipse(pos.x,pos.y,this.width,this.height);
+    }
+};
